45fb0f31c26668bc2a55c4bb9b999f3dc246ffda
ysu3 <ysu3@uvm.edu>
2019-04-10 17:26:13 +1000

    add function highestRevenueLaunchServiceProviders

---
+++ src/main/java/rockets/mining/RocketMiner.java
@@ -0,0 +1,119 @@
+package rockets.mining;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rockets.dataaccess.DAO;
+import rockets.model.Launch;
+import rockets.model.LaunchServiceProvider;
+import rockets.model.Rocket;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
+import java.util.Set;
+import java.util.LinkedHashSet;
+
+
+public class RocketMiner {
+    private static Logger logger = LoggerFactory.getLogger(RocketMiner.class);
+
+    private DAO dao;
+    private ArrayList<String> countriesWithLSPThisYear;
+
+    public RocketMiner(DAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * TODO: to be implemented & tested!
+     * Returns the top-k most active rockets, as measured by number of completed launches.
+     *
+     * @param k the number of rockets to be returned.
+     * @return the list of k most active rockets.
+     */
+    public List<Rocket> mostLaunchedRockets(int k) {
+        return null;
+    }
+
+    /**
+     * TODO: to be implemented & tested!
+     * <p>
+     * Returns the top-k most reliable launch service providers as measured
+     * by percentage of successful launches.
+     *
+     * @param k the number of launch service providers to be returned.
+     * @return the list of k most reliable ones.
+     */
+    public List<LaunchServiceProvider> mostReliableLaunchServiceProviders(int k) {
+        return null;
+    }
+
+    /**
+     * <p>
+     * Returns the top-k most recent launches.
+     *
+     * @param k the number of launches to be returned.
+     * @return the list of k most recent launches.
+     */
+    public List<Launch> mostRecentLaunches(int k) {
+        logger.info("find most recent " + k + " launches");
+        Collection<Launch> launches = dao.loadAll(Launch.class);
+        Comparator<Launch> launchDateComparator = (a, b) -> -a.getLaunchDate().compareTo(b.getLaunchDate());
+        return launches.stream().sorted(launchDateComparator).limit(k).collect(Collectors.toList());
+    }
+
+    /**
+     * TODO: to be implemented & tested!
+     * <p>
+     * Returns the dominant country who has the most launched rockets in an orbit.
+     *
+     * @param orbit the orbit
+     * @return the country who sends the most payload to the orbit
+     */
+    public String dominantCountry(String orbit) { return null;}
+
+    /**
+     * TODO: to be implemented & tested!
+     * <p>
+     * Returns the top-k most expensive launches.
+     *
+     * @param k the number of launches to be returned.
+     * @return the list of k most expensive launches.
+     */
+    public List<Launch> mostExpensiveLaunches(int k) {
+        return null;
+    }
+
+    /**
+     * TODO: to be implemented & tested!
+     * <p>
+     * Returns a list of launch service provider that has the top-k highest
+     * sales revenue in a year.
+     *
+     * @param k the number of launch service provider.
+     * @param year the year in request
+     * @return the list of k launch service providers who has the highest sales revenue.
+     */
+    public List<LaunchServiceProvider> highestRevenueLaunchServiceProviders(List<LaunchServiceProvider> lsps, int k, int year) {
+        for(int i=0; i<k; i++){
+            if(lsps.get(i).getYearFounded() == year){
+                countriesWithLSPThisYear.add(lsps.get(i).getCountry());
+            }
+        }
+        Collections.sort(countriesWithLSPThisYear);
+        for(int i=0; i<k; i++){
+            if(lsps.get(i).getCountry() == countriesWithLSPThisYear.get(0)){
+                LaunchServiceProvider highestRevenueLaunchServiceProvider = new LaunchServiceProvider(lsps.get(i).getName(), lsps.get(i).getYearFounded(), lsps.get(i).getCountry());
+                lsps.clear();
+                lsps.add(highestRevenueLaunchServiceProvider);
+            }
+        }
+        Set<LaunchServiceProvider> s = new LinkedHashSet<>(lsps);
+
+        return lsps;
+    }
+}
